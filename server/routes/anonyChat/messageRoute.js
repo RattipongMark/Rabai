2c892385aaaef03b7a996afc409943f9fa58d482
Nawin Tosilanon <nawintosilanon@gmail.com>
2024-11-14 21:52:28 +0700

    getallcommnet

--- server/routes/anonyChat/messageRoute.js
+++ server/routes/anonyChat/messageRoute.js
@@ -3,7 +3,7 @@ const messageController = require('../../controllers/anonyChat/messageController
 
 const router = express.Router();
 
-router.post('/', messageController.createMessage); 
+router.post('/', messageController.createMessage);
 router.get('/:room', messageController.getMessagesByRoom); 
 router.delete('/:room', messageController.deleteMessagesByRoom); 
 
